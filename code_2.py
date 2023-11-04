b2c2fabac7911fc52e30d7a5959d0bbee5b946e6
Pavel <143156330+pavel844@users.noreply.github.com>
2023-11-04 23:47:39 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,43 @@
+    });
+
+}
+    }
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    const data = [];
+
+    data.forEach(item => {
+function generateRandomData() {
+}
+function main() {
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    return data;    for item in data:
+    return data
+    main()
+
+
+import random
+
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+if __name__ == "__main__":
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    for item in data:
+
+    return data
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+import random
+
+def main():
+if __name__ == "__main__":
+    data = generate_random_data()
+def generate_random_data():
